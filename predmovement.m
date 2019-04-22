c6c3c585867d6a0e2aa48ffccda3049715fe3bb1
Spyridon Couvaras <s.couvaras@se16.qmul.ac.uk>
2019-04-22 18:32:38 +0100

    newWPF

--- predmovement.m
+++
@@ -1,9 +0,0 @@
-function [y] = predmovement(x)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-load workspace_head.mat
-
-y=predict(Md1,x);
-
-end
-
--- predmovement_2.m
+++
@@ -1,9 +0,0 @@
-function [y] = predmovement_2(x)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-load new_workspace_2.mat
-
-y=predict(Md1,x);
-
-end
-
